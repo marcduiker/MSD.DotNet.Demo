2a3360228adfc0decf4e99640bd62b163e1ff211
Marc Duiker <marcduiker@gmail.com>
2017-06-17 18:25:37 +0200

    Added basic calculator class without dependencies

---
+++ MSD.DotNet.Patterns/MSD.DotNet.Patterns.TestableCodeRefactoring.UnitTests/Basic/CalculatorTests.cs
@@ -0,0 +1,85 @@
+﻿using MSD.DotNet.Patterns.TestableCodeRefactoring.Basic;
+using NUnit.Framework;
+
+namespace MSD.DotNet.Patterns.TestableCodeRefactoring.UnitTests.Basic
+{
+    /// <summary>
+    /// Class with unit tests for the Calculator which uses the StaticLogger.
+    /// </summary>
+    public class CalculatorTests
+    {
+        [Test]
+        public void Add_SingleNumber_ReturnsTheSameNumber()
+        {
+            // Arrange
+            var calculator = GetCalculator();
+
+            // Act
+            calculator.Add(1);
+
+            // Assert
+            Assert.AreEqual(1d, calculator.Value);
+        }
+
+        [Test]
+        public void Add_TwoNumbers_ReturnsTheSumOfTheNumbers()
+        {
+            // Arrange
+            var calculator = GetCalculator();
+
+            // Act
+            calculator.Add(1);
+            calculator.Add(2);
+
+            // Assert
+            Assert.AreEqual(3d, calculator.Value);
+        }
+
+        [Test]
+        public void Substract_SinglePositiveNumber_ReturnsTheNegativeOfTheNumber()
+        {
+            // Arrange
+            var calculator = GetCalculator();
+
+            // Act
+            calculator.Substract(1);
+
+            // Assert
+            Assert.AreEqual(-1d, calculator.Value);
+        }
+
+        [Test]
+        public void Substract_TwoNumbers_ReturnsTheDifferenceBetweenTheNumbers()
+        {
+            // Arrange
+            var calculator = GetCalculator();
+
+            // Act
+            calculator.Add(3);
+            calculator.Substract(2);
+
+            // Assert
+            Assert.AreEqual(1d, calculator.Value);
+        }
+
+        [Test]
+        public void Clear_AfterCalculation_ReturnsZero()
+        {
+            // Arrange
+            var calculator = GetCalculator();
+
+            // Act
+            calculator.Add(1);
+            calculator.Add(2);
+            calculator.Clear();
+
+            // Assert
+            Assert.AreEqual(0d, calculator.Value);
+        }
+
+        private static Calculator GetCalculator()
+        {
+            return new Calculator();
+        }
+    }
+}
---
+++ MSD.DotNet.Patterns/MSD.DotNet.Patterns.TestableCodeRefactoring/Basic/Calculator.cs
@@ -0,0 +1,25 @@
+﻿namespace MSD.DotNet.Patterns.TestableCodeRefactoring.Basic
+{
+    /// <summary>
+    /// Basic Calculator class without any dependencies.
+    /// </summary>
+    public class Calculator
+    {
+        public decimal Value { get; set; }
+
+        public void Add(decimal value)
+        {
+            Value += value;
+        }
+
+        public void Substract(decimal value)
+        {
+            Value -= value;
+        }
+
+        public void Clear()
+        {
+            Value = 0;
+        }
+    }
+}
